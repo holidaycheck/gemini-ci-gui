cd947e5d6022c3da41fa83c2d7ed4fd03e198f1b
Mathias Schreck <schreck.mathias@googlemail.com>
2016-10-06 10:46:35 +0200

    Allow port to be configured via environment variable

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 'use strict';
 
 /* eslint-disable no-process-env */
+/* eslint-disable no-console */
 
 const createServer = require('./createServer');
 
 const reportFile = process.env.REPORT_FILE;
+const port = process.env.PORT || 3000;
 const server = createServer(reportFile);
 
-server.listen(3000);
+server.listen(port, function () {
+    console.log(`Listening on port ${port}`);
+});
