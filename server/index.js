d8024d3611f124c0cc356eda56039cd91304f203
Mathias Schreck <schreck.mathias@googlemail.com>
2016-10-06 10:47:17 +0200

    Throw when REPORT_FILE is not defined

--- server/index.js
+++ server/index.js
@@ -9,6 +9,10 @@ const reportFile = process.env.REPORT_FILE;
 const port = process.env.PORT || 3000;
 const server = createServer(reportFile);
 
+if (!reportFile) {
+    throw new Error('REPORT_FILE is not defined');
+}
+
 server.listen(port, function () {
     console.log(`Listening on port ${port}`);
 });
